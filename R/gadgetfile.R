gadget-framework/rgadget

commit 0f450c24655e4c1fccf021158b8aebd3a3fb27a9

gadgetfile: If no string parts involve brackets, dont try to re-group
No brackets at all is going to be the common case for gadget files, so optimise
for that and skip the split/join step.

R/gadgetfile.R
@@ -20,6 +20,10 @@ split_gadgetfile_line <- function (line) {
   parts <- unlist(strsplit(line, "\\s+"))
   # Compare count of opening brackets and closing brackets in parts
   stack <- stringi::stri_count_fixed(parts, "(") - stringi::stri_count_fixed(parts, ")")
+
+  # If there aren't any brackets to balance, return split parts now
+  if (all(stack == 0)) return(parts)
+
   # 0 ==> part of an unclosed expression, 1 ==> combine with any previous 0 parts
   stack <- c(1, ifelse(cumsum(stack) > 0, 0, 1))[1:length(stack)]
   # Use this as a factor to split up the parts into groups of whole expressions

commit 32014a1a7e69f1284c1d922730ca95c984c50e0e

gadgetfile: split_gadgetfile_line("") should equal ""
This was broken by 0f450c2, strsplit("") ==> c() but this was being hidden by
the stack munging. Add a specific case for this.

R/gadgetfile.R
@@ -14,6 +14,9 @@ split_gadgetfile_line <- function (line) {
     paste(string, collapse = " ")
   }
   
+  # Empty line should be returned untouched (NB: strsplit will return c() in this case)
+  if (line == "") return("")
+
   # Remove all comments
   line <- gsub(';.+', '', line)
   # Split by any whitespace first