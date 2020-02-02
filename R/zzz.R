2b732ae5564322e549f0bd79eb44cae09e55239f
bthe <bjarki.elvarsson@gmail.com>
2020-02-02 22:19:14 +0000

    Added warning for deprecated functions

--- R/zzz.R
+++ R/zzz.R
@@ -7,3 +7,17 @@
   
   
 }
+
+#' Deprecated functions
+#' @rdname deprecated_functions
+#' @param ... unused inputs
+#' @export
+gadget.forward <- function(...){
+  stop('-- gadget.forward is deprecated, please adjust your code to gadget_project_* functions --')
+}
+
+#' @rdname deprecated_functions
+#' @export
+gadget.ypr<- function(...){
+  stop('-- gadget.ypr is deprecated, please adjust your code to gadget_project_* functions --')
+}
