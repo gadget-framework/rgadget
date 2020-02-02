1de8d27edad51d3f387b4aaf86ecda06d090ea3a
Jamie Lentin <jm@lentin.co.uk>
2020-02-02 19:43:12 +0000

    helpers: Rewrite von_b_formula to not require infuser
    infuser got removed from CRAN for whatever reason, and it's only use is better
    written by manipulating R expressions directly with substitute().

--- R/helpers.R
+++ R/helpers.R
@@ -10,12 +10,16 @@
 #'
 #' @return vector of von B formula by age
 #' @export
-von_b_formula <- function(a,linf='Linf',k='k',recl='recl'){
-  a %>% 
-    purrr::map(~infuser::infuse("{{linf}} * (1 - exp(-1 * (0.001 * {{k}}) * ({{a}} - (1 + log(1 - {{recl}}/{{linf}})/(0.001 * {{k}})))))",
-                         a=.,linf=linf,k=k,recl=recl)) %>% 
-    purrr::map(~parse(text=.) %>% 
-          map(to.gadget.formulae)) %>% 
-    unlist()
+von_b_formula <- function(a, linf='Linf', k='k', recl='recl') {
+    vapply(a, function (age) {
+        to.gadget.formulae(substitute(Linf * (1 - exp(
+            -1 *
+            (0.001 * k) *
+            (age - (1 + log(1 - recl/Linf)/(0.001 * k)))
+        )), list(
+            Linf = as.symbol(linf),
+            k = as.symbol(k),
+            recl = as.symbol(recl),
+            age = as.symbol(age))))
+    }, character(1)) %>% unname()
 }
-
---
+++ tests/test-helpers.R
@@ -0,0 +1,15 @@
+library(Rgadget)
+library(unittest, quietly = TRUE)
+library(magrittr)
+
+ok_group("von_b_formula", {
+    ok(ut_cmp_identical(von_b_formula('argle'),
+        "(* #Linf (- 1 (exp (* (* (- 1) (* 0.001 #k)) (- #argle (+ 1 (/ (log (- 1 (/ #recl #Linf))) (* 0.001 #k))))))))"), "Generated Vb with default variable names")
+    ok(ut_cmp_identical(von_b_formula('argle', linf='elll', k='kay', recl='rrr'),
+        "(* #elll (- 1 (exp (* (* (- 1) (* 0.001 #kay)) (- #argle (+ 1 (/ (log (- 1 (/ #rrr #elll))) (* 0.001 #kay))))))))"), "Generated Vb with custom variable names")
+    ok(ut_cmp_identical(von_b_formula(c("a","b","c")), c(
+        "(* #Linf (- 1 (exp (* (* (- 1) (* 0.001 #k)) (- #a (+ 1 (/ (log (- 1 (/ #recl #Linf))) (* 0.001 #k))))))))",
+        "(* #Linf (- 1 (exp (* (* (- 1) (* 0.001 #k)) (- #b (+ 1 (/ (log (- 1 (/ #recl #Linf))) (* 0.001 #k))))))))",
+        "(* #Linf (- 1 (exp (* (* (- 1) (* 0.001 #k)) (- #c (+ 1 (/ (log (- 1 (/ #recl #Linf))) (* 0.001 #k))))))))",
+        NULL)), "Generated multiple formulae per a")
+})
